398bf77a978323b36293cbaf3cb66a9ce9c0794e
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-11-06 21:48:31 +0100

    find, findbyid

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const SchemeRouter = require('./schemes/scheme-router.js');
 
 const server = express();
 
+server.use(logger)
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
+
+function logger(req, res, next) {
+    console.log(`${req.method} to ${req.originalUrl}`)
+    next();
+  }
 
 module.exports = server;
